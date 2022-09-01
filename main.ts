07ae3a1d57ad3c84d7c953a540e109686a04c156
HamishNakatomi <60995335+HamishNakatomi@users.noreply.github.com>
2022-09-01 16:49:41 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    let last_time = press_time
+    last_time = press_time
     press_time = input.runningTime()
     delta = press_time - last_time
-    predicted_delta = predicted_delta * 0.5 + delta * 0.5
+    predicted_delta = predicted_delta * update_rate + delta * (1 - update_rate)
 })
 function flash () {
     basic.showLeds(`
@@ -24,7 +24,10 @@ function flash () {
 let flash_time = 0
 let predicted_delta = 0
 let delta = 0
+let last_time = 0
 let press_time = 0
+let update_rate = 0
+update_rate = 0.9
 press_time = 0
 basic.forever(function () {
     if (flash_time + predicted_delta < input.runningTime()) {
